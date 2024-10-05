20d127f571bd5ce99d04e5a4870d5d5947aaa31c
Almyr <almyr.abath@ymail.com>
2024-10-05 14:02:19 -0300

    Create works_on.sql

---
+++ works_on.sql
@@ -0,0 +1,27 @@
+create table works_on(
+	Essn char(9) not null,
+    Pno int not null,
+    Hours decimal(3,1) not null,
+    primary key (Essn, Pno),
+    constraint fk_employee_works_on foreign key (Essn) references employee(Ssn),
+    constraint fk_project_works_on foreign key (Pno) references project(Pnumber)
+);
+
+
+insert into works_on values
+(123456789, 1, 32.5),
+(123456789, 2, 7.5),
+(666884444, 3, 40.0),
+(453453453, 1, 20.0),
+(453453453, 2, 20.0),
+(333445555, 2, 10.0),
+(333445555, 3, 10.0),
+(333445555, 10, 10.0),
+(333445555, 20, 10.0),
+(999887777, 30, 30.0),
+(999887777, 10, 10.0),
+(987987987, 10, 35.0),
+(987987987, 30, 5.0),
+(987654321, 30, 20.0),
+(987654321, 20, 15.0),
+(888665555, 20, 0.0);
