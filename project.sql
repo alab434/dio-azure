18e4986deb4235dcdf4c12f5765f13808e366320
Almyr <almyr.abath@ymail.com>
2024-10-05 14:01:52 -0300

    Create project.sql

---
+++ project.sql
@@ -0,0 +1,17 @@
+create table project(
+	Pname varchar(15) not null,
+	Pnumber int not null,
+    Plocation varchar(15),
+    Dnum int not null,
+    primary key (Pnumber),
+    constraint unique_project unique (Pname),
+    constraint fk_project foreign key (Dnum) references departament(Dnumber)
+);
+
+insert into project values
+('ProductX', 1, 'Bellaire', 5),
+('ProductY', 2, 'Sugarland', 5),
+('ProductZ', 3, 'Houston', 5),
+('Computerization', 10, 'Stafford', 4),
+('Reorganization', 20, 'Houston', 1),
+('Newbenefits', 30, 'Stafford', 4);
